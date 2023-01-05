a62d4cf65901462ec063ad601bd8e28f3b09eead
benNjogu <kamandemachafi@gmail.com>
2023-01-05 09:52:15 +0300

    My initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+
+import images from "../../constants/images";
+import "./Navbar.css";
+
+//BEM block element modifier
+const Navbar = () => <nav className="app__navbar"></nav>;
+
+export default Navbar;
